ca0101802ae383a8dca5eb1b58c042cc37163fda
abhishek <abhishek@thinksys.com>
2021-12-01 22:42:42 +0530

    script tag added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx:any) {
+  static async getInitialProps(ctx: any) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
@@ -9,13 +10,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3552440748579508"
-            crossOrigin="anonymous"
-          ></script>
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
@@ -24,15 +19,17 @@ class MyDocument extends Document {
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3552440748579508"
             crossOrigin="anonymous"
           ></script>
-
           <ins
             className="adsbygoogle"
+            style={{ display: "inline-block", width: 300, height: 250 }}
             data-ad-client="ca-pub-3552440748579508"
             data-ad-slot="8923990174"
             data-ad-format="auto"
             data-full-width-responsive="true"
           ></ins>
-          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+          <Script strategy="beforeInteractive">
+            (adsbygoogle = window.adsbygoogle || []).push({});
+          </Script>
         </body>
       </Html>
     );
